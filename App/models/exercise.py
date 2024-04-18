ae80452b930e73dd45c1f3cdab5ef4b8c4842572
knjprojects <joshthereactdev@gmail.com>
2024-04-18 11:38:49 -0400

    updated users, loading exercises

--- App/models/exercise.py
+++ App/models/exercise.py
@@ -6,16 +6,15 @@ class Exercise(db.Model):
     muscle = db.Column(db.String(50),nullable=False)
     equipment = db.Column(db.String(100),nullable=False)
     difficulty = db.Column(db.String(50),nullable=False)
-    instructions = db.Column(db.String(255),nullable=False)
+    #instructions = db.Column(db.String(255),nullable=False)
     image = db.Column(db.String(200),nullable=False)
 
-    def __init__(self, name, description, muscle, equipment, difficulty, instructions, image):
+    def __init__(self, name, description, muscle, equipment, difficulty, image):
         self.name = name
         self.description = description
         self.muscle = muscle
         self.equipment = equipment
         self.difficulty = difficulty
-        self.instructions = instructions
         self.image = image
 
     def get_json(self):
@@ -26,6 +25,5 @@ class Exercise(db.Model):
             'muscle': self.muscle,
             'equipment': self.equipment,
             'difficulty': self.difficulty,
-            'instructions': self.instructions,
             'image': self.image
         }
