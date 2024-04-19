4f11d3f700422e6ae7474058e89e1cb8b488ca69
knjprojects <joshthereactdev@gmail.com>
2024-04-19 17:49:39 -0400

    removed mealfootitem and revamped models

---
+++ App/controllers/calendar.py
@@ -0,0 +1,5 @@
+from App.databse import db
+from App.models import CalendarIntegration
+
+def createCalendar(date,user_id,timezone):
+    return None
--- App/models/calendarintegration.py
+++ App/models/calendarintegration.py
@@ -5,16 +5,17 @@ class CalendarIntegration(db.Model):
     routines = db.relationship('RoutineCalendar', backref='calendar_integration', lazy=True)
     meals = db.relationship('MealCalendar', backref='calendar_integration', lazy=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __init__(self, date, user_id):
+    timezone=db.Column(db.String(100), nullable=False)
+    def __init__(self, date, user_id, timezone):
         self.date = date
         self.user_id = user_id
-
+        self.timezone=timezone
     def get_json(self):
         return {
             'id': self.id,
             'date': self.date,
             'user_id': self.user_id,
+            'timezone':self.timezone,
             'routines': [routine_calendar.get_json() for routine_calendar in self.routines],
             'meals': [meal_calendar.get_json() for meal_calendar in self.meals]
         }
