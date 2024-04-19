b9e3117529a92cb0b7cf42dc00b805e44f6b85d9
knjprojects <joshthereactdev@gmail.com>
2024-04-18 21:49:06 -0400

    updated users, loading exercises, updated routines

--- App/controllers/routineexercise.py
+++ App/controllers/routineexercise.py
@@ -1,5 +1,5 @@
 from App.database import db
-from App.models import Routine, Exercise, User
+from App.models import Routine, Exercise, User, ExerciseRoutine
 
 """def test_routine_exercise_relationship():
     user = User.query.get(1)
@@ -14,3 +14,33 @@ from App.models import Routine, Exercise, User
     assert len(loaded_routine.exercises) == 2
     assert loaded_routine.exercises[0].id == exercise1.id
     assert loaded_routine.exercises[1].id == exercise2.id"""
+
+
+def addExerciseToRoutine(routine_id,exercise_id):
+        try:
+                routineexercise=ExerciseRoutine(routine_id=routine_id, exercise_id=exercise_id)
+                db.session.add(routineexercise)
+                db.session.commit()
+                return routineexercise
+        except Exception as e:
+                print(e)
+                db.session.rollback()
+                return None
+        
+#all exercises in all routines
+def list_routine_exercises():
+        rxs=ExerciseRoutine.query.all()
+        if not rxs:
+                return []
+       
+        rx_list = [rx.get_json() for rx in rxs]
+        return rx_list
+        
+
+#all exercises for a specific routine
+def list_routine_exercise_by_routine(routine_id):
+        rxs=ExerciseRoutine.query.filter_by(routine_id=routine_id)
+        if not rxs:
+                return None
+        rxs_list=[rx.get_json() for rx in rxs]
+        
