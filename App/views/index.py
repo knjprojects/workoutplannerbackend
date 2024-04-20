05b21a5e222a22d00e0582bd496ebf20abc674d6
knjprojects <joshthereactdev@gmail.com>
2024-04-19 21:41:11 -0400

    testing calendar creation

--- App/views/index.py
+++ App/views/index.py
@@ -1,7 +1,7 @@
 import csv
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.database import db
-from App.controllers import create_user,create_test_users, createRoutine,createMeal,getMeals,mealsForUser,getFoodById
+from App.controllers import create_user,create_test_users, createRoutine,createMeal,getMeals,mealsForUser,getFoodById,createCalendar,get_user_calendars
 from App.controllers import login, create_book, create_review,loadExercises,loadFoods,list_foods,list_exercises,list_routines,get_user_routines
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
@@ -35,7 +35,8 @@ def init():
         #userid=user1.id
         createRoutine(user1.id, 'Abs Workout', 'I want to build abs', "['Meat', 'Veggies']","['Weight Loss', 'Building Muscle']")
         createMeal(user1.id,1)
-
+        createCalendar(date='04-12-2016',user_id=user1.id,timezone='AST')
+    
         #prefs="", fgoals=""
     loadExercises()
     loadFoods()
@@ -77,8 +78,8 @@ def user_meals(user_id):
 
 
 @index_views.route('/calendars/<int:user_id>', methods=['GET'])
-def user_calendars():
-    return jsonify(getMeals())
+def user_calendars(user_id):
+    return jsonify(get_user_calendars(userid=user_id))
 
 
 @index_views.route('/health', methods=['GET'])
