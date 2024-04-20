c897d79d9bab7e4861b93828a5889e878497a059
knjprojects <joshthereactdev@gmail.com>
2024-04-19 22:40:10 -0400

    testing calendar creation

--- App/models/meal.py
+++ App/models/meal.py
@@ -4,6 +4,9 @@ class Meal(db.Model):
     food_item_id = db.Column(db.Integer, db.ForeignKey('food_item.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    food_item = db.relationship('FoodItem', backref='meals', lazy=True)
+    user = db.relationship('User', backref='meals', lazy=True)
+
     def __init__(self, food_item_id, user_id):
         self.food_item_id = food_item_id
         self.user_id = user_id
@@ -12,5 +15,7 @@ class Meal(db.Model):
         return {
             'id': self.id,
             'food_item_id': self.food_item_id,
-            'user_id': self.user_id
+            'user_id': self.user_id,
+            'food_item': self.food_item.get_json() if self.food_item else None,
+            'user': self.user.get_json() if self.user else None
         }
