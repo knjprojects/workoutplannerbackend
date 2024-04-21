bed88f6e5001f3d49434f0ba9b93abc10da6e30a
knjprojects <joshthereactdev@gmail.com>
2024-04-20 21:26:39 -0400

    checking for saved calendar before returning to cal in /

--- App/controllers/mealcalendar.py
+++ App/controllers/mealcalendar.py
@@ -7,8 +7,8 @@ def createMealCalendarEntry(date, user_id, meal_id, calendar_integration_id):
                 
                 db.session.add(meal_calendar)
                 db.session.commit()
-                mcel=MealCalendar.query.filter_by(user_id=user_id).first()
-                return  mcel.get_json() #meal_calendar
+                #mcel=MealCalendar.query.filter_by(user_id=user_id,date=date).first()
+                return  meal_calendar.get_json() #meal_calendar
     except Exception as e:
                 print(e)
                 db.session.rollback()
